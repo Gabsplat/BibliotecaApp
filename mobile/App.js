dba5bcb039a97237d14cd24a31e20e260e094443
Gabriel Carrillo <agabrielcarrillo01@gmail.com>
2024-05-31 20:56:54 -0300

    hola

--- mobile/App.js
+++ mobile/App.js
@@ -5,7 +5,7 @@ import { Alert, StyleSheet, Text } from "react-native";
 import { TamaguiProvider, View, Button, createTamagui } from "tamagui";
 import { config } from "@tamagui/config/v3";
 
-// you usually export this from a tamagui.config.ts file
+// you usually export this from a tamagui.config.ts filesss
 const tamaguiConfig = createTamagui(config);
 
 export default function App() {
